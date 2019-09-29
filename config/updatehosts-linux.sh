8e41721bbe0f2f5c7a320647cc0352db63138965
jr <jr>
2019-09-29 19:13:33 -0300

    update hosts script

---
+++ config/updatehosts-linux.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "127.0.0.1 wc2019.dckr" >> /etc/hosts
---
+++ config/updatehosts-win.bat
@@ -0,0 +1,5 @@
+@echo off 
+
+echo 127.0.0.1 wc2019.dckr >> %windir%\System32\drivers\etc\hosts
+echo 192.168.33.10 wc2019.vgrt >> %windir%\System32\drivers\etc\hosts
+exit
